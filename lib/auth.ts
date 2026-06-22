4a1b74147321e7815aeacb7fae1a468235114a97
t0wk1r <techtowkir@gmail.com>
2026-06-22 11:49:28 +0600

    auth ts file modify for goolge login

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,11 @@ export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
 
+  trustedOrigins: [
+    "http://localhost:3000",
+    process.env.CLIENT_URL,
+  ].filter(Boolean) as string[],
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
