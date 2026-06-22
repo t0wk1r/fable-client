ab4345b1c93d19df57034f752807d9a557188711
t0wk1r <techtowkir@gmail.com>
2026-06-22 21:13:24 +0600

    auth file modifed

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,13 @@
-"use client";
+import { betterAuth } from "better-auth";
 
-import { createAuthClient } from "better-auth/react";
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
 
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
